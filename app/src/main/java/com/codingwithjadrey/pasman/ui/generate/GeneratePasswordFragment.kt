f8fca9ec0328ca3240df31a28d667094e448ab53
Isaac Jadrey <jadreyisaac@gmail.com>
2022-12-30 16:31:12 +0300

    implemented the generate password functionality in generatePasswordFragment

--- app/src/main/java/com/codingwithjadrey/pasman/ui/generate/GeneratePasswordFragment.kt
+++ app/src/main/java/com/codingwithjadrey/pasman/ui/generate/GeneratePasswordFragment.kt
@@ -1,10 +1,14 @@
 package com.codingwithjadrey.pasman.ui.generate
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.codingwithjadrey.pasman.databinding.FragmentGeneratePasswordBinding
+import com.codingwithjadrey.pasman.util.makeToast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 /**
@@ -47,8 +51,41 @@ class GeneratePasswordFragment: BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            generatePasswordBtn.setOnClickListener { generatePassword(12) }
+            copyToClipboardBtn.setOnClickListener { copyToClipboard() }
+        }
     }
 
+    /** method that generates a random password with a specified character
+     * length when invoking the method */
+    private fun generatePassword(i: Int) {
+        val characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%&^*/()|?"
+        val stringBuilder = StringBuilder(i)
+
+        repeat(i) {
+            val generatedString = (characters.indices).random()
+            stringBuilder.append(characters[generatedString])
+        }
+
+        stringBuilder.insert((0 until i).random(), "")
+        binding.apply {
+            generatedPassword.text = stringBuilder.toString()
+            copyToClipboardBtn.visibility = View.VISIBLE
+        }
+    }
+
+    /** method to copy the generated password string into the system clipboard
+     * so that it can be copied into the password field */
+    private fun copyToClipboard() {
+        val clipboard = requireActivity().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipboardContent  = ClipData.newPlainText("", binding.generatedPassword.text.toString())
+        clipboard.setPrimaryClip(clipboardContent)
+        makeToast(requireContext(), "Copied to clipboard")
+        dialog?.dismiss()
+    }
+
+    /** called when the view is destroyed */
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
