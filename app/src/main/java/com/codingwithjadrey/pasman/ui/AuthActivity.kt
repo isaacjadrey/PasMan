94d450ea5fe98387fa79fa534a83d9ead7ac3f91
Isaac Jadrey <jadreyisaac@gmail.com>
2022-12-31 11:52:17 +0300

    created an authActivity to provide secure access to the app

---
+++ app/src/main/java/com/codingwithjadrey/pasman/ui/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.codingwithjadrey.pasman.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codingwithjadrey.pasman.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
