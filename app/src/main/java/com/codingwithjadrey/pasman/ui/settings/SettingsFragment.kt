f8c79bf4b1b8b0d343652de212125070b9179790
Isaac Jadrey <jadreyisaac@gmail.com>
2023-01-04 16:01:11 +0300

    implemented info dialog in settingsFragment

--- app/src/main/java/com/codingwithjadrey/pasman/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/codingwithjadrey/pasman/ui/settings/SettingsFragment.kt
@@ -1,15 +1,19 @@
 package com.codingwithjadrey.pasman.ui.settings
 
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.codingwithjadrey.pasman.BuildConfig
 import com.codingwithjadrey.pasman.databinding.FragmentSettingsBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class SettingsFragment: Fragment() {
+class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
@@ -50,6 +54,24 @@ class SettingsFragment: Fragment() {
     }
 
     private fun infoDialog() {
+        val url = "https://github.com/isaacjadrey/PasMan"
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+
+        val versionNumber = BuildConfig.VERSION_NAME
+        MaterialAlertDialogBuilder(requireContext()).apply {
+            setTitle("About")
+            setMessage(
+                "Pas-Man is your personal password manager, " +
+                        "save all your passwords right on the move." +
+                        "\nNo need to forget your passwords anymore." +
+                        "\n\nVersion: $versionNumber" +
+                        "\nPowered by COJ"
+            )
+            setNeutralButton("Visit Site") { _, _ ->
+                startActivity(intent)
+            }
+        }.show()
     }
 
     /** called when the view is destroyed */
